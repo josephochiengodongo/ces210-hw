2a92c1d7b1a8ac9250af5e8527ec12abafdb5175
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-04 17:20:53 +0300

    modified:   .vscode/launch.json 	modified:   week01/Exercise1/Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string first =Console.ReadLine();
+
+        Console.WriteLine("What is your second name?");
+        string second=Console.ReadLine();
+
     }
 }
