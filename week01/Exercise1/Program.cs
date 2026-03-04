d0593932d12fb98910544f48c87875bcfff139a1
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-04 17:28:19 +0300

    modified:   week01/Exercise1/Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -7,7 +7,8 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
         Console.Write("What is your first name? ");
         string first =Console.ReadLine();
-        Console.WriteLine("What is your second name?");
+
+        Console.Write("What is your second name?");
         string second=Console.ReadLine();
 
     }
