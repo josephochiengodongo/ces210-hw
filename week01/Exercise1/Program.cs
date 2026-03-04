2325c6e8b3a311704f87d0a80e7e92ec2cf73cf0
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-04 18:06:08 +0300

    modified:   week01/Exercise1/Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -11,5 +11,7 @@ class Program
         Console.Write("What is your last name? ");
         string last =Console.ReadLine();
 
+        Console.WriteLine($"Your name is {last},{first} {last}.");
+
     }
 }
