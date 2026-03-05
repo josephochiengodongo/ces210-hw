aad65bd9816ac22642f2b400915671981ad46707
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-05 18:34:31 +0300

    effect

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -38,7 +38,7 @@ class Program
         //strech challege: determine + 
         string sign = "";
 
-        int lastDigit =percent % 10;
+        int lastDigit =grade % 10;
 
         if (lastDigit >= 7)
         {
