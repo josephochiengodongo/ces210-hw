4605a7dc4e8f30a37fed293dee5d3f5d4ece4063
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-05 15:53:32 +0300

    effect

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -9,9 +9,40 @@ class Program
         string answer =Console.ReadLine();
         int percent = int.Parse(answer);
 
+
         string letter = "";
 
-        if (percent >=90)
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        else if (percent >= 80)
+        {
+            letter="B";
+        }
+        else if (percent >=70)
+        {
+            letter="C";
+        }
+        else if (percent >=60)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+        }
+        Console.WriteLine($"Your grade is:{letter}");
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time")
+        }
+    
         
     }
 }
+    
