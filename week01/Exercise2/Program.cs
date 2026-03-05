004ad41f5103c6ad1c3448c00c7f55dbdc0a3fa6
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-05 15:24:09 +0300

    effect

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,13 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        string answer =Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >=90)
+        
     }
 }
