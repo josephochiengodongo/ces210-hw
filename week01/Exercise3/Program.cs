74782c81366a4e02cfb64da155cbaae343266db0
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-09 20:46:43 +0300

    effect

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
@@ -7,54 +8,52 @@ class Program
         Console.WriteLine("Hello World! This is the Exercise3 Project.");
 
         //First core (no loop)
-        Console.Write("What is the margic number?  ");
-        int magicNumber = int.Parse(Console.ReadLine());
+       //Console.Write("What is the margic number?  ");
+       //int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess?    ");
-        int guess = int.Parse(Console.ReadLine());
+        //Console.Write("What is your guess?    ");
+        //int guess = int.Parse(Console.ReadLine());
     
 
-            if (magicNumber > guess)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if (magicNumber < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it!");
-            }
+        //    if (magicNumber > guess)
+        //    {
+        //        Console.WriteLine("Higher");
+        //    }
+        //    else if (magicNumber < guess)
+        //    {
+        //        Console.WriteLine("Lower");
+        //    }
+         //   else
+        //    {
+        //        Console.WriteLine("You guessed it!");
+         //   }
             //second core (loop)
-            while (guess != magicNumber)
-            {
-              Console.Write("What is your guess?  ");
-              guess = int.Parse(Console.ReadLine()); 
+        //    while (guess != magicNumber)
+         //   {
+        //      Console.Write("What is your guess?  ");
+        //      guess = int.Parse(Console.ReadLine()); 
 
-              if (guess < magicNumber)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if(guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You guessed it");
-            }
+         //     if (guess < magicNumber)
+         //   {
+        //        Console.WriteLine("Higher");
+        //    }
+         //   else if(guess > magicNumber)
+         //   {
+         //      Console.WriteLine("Lower");
+        //  }
+         //   else
+         //  {
+           //  Console.WriteLine("You guessed it");
+        //  }
 
-        }     
-        // Third core, where we use a random number
+       // }     
+        // Third core, where we use a random number.
         Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,101);
+
+        int guess = -1;
     
-        magicNumber = randomGenerator.Next(1, 101);
-        Console.Write("What is your guess? ");
-        guess = int.Parse(Console.ReadLine());
-        
-        // We could also use a do-while loop here...
-        while (guess != magicNumber)
+            while (guess != magicNumber)
         {
             Console.Write("What is your guess? ");
             guess = int.Parse(Console.ReadLine());
--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -6,4 +6,4 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
     }
-}
+}  
