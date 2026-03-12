442b73bc8dc4e422162181415fdf5e1c05e4333d
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-12 17:46:59 +0300

    effect

--- week02/Journal/Entry.cs
+++ week02/Journal/Entry.cs
@@ -3,13 +3,13 @@ public class Entry
 {
     public string _date;
     public string _promptText;
-    public string _response;
+    public string _entryText;
 
     public void Display()
     {
       Console.WriteLine($"date");
       Console.WriteLine($"prompt: {_promptText}");
-      Console.WriteLine($"response: {_response}"); 
+      Console.WriteLine($"response: {_entryText}"); 
       Console.WriteLine();
     }
 }
