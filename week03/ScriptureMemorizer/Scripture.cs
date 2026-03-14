8b1980c0e701cd98ebc40cb096a018e5ed150b84
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-14 05:14:52 +0300

    effect

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+  private Reference _reference;
+  private List<Word> _words;
+  private Random _random;
+
+  public Scripture(Reference reference, string text)
+    {
+      _reference = reference;
+      _words = new List<Word>();
+
+      string[] splitWords = text.Split ('');
+
+      foreach (string word in splitWords)
+        {
+            _words.Add(new Word(word));
+        }
+    }
+    
+        
+        
+        public void HideRandomWords(int numberToHide)
+    {
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = _random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        string result = _reference.GetDisplayText() + " ";
+
+        foreach (Word word in _words)
+        {
+            result += word.GetDisplayText() + " ";
+        }
+
+        return result;
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+            
+    
