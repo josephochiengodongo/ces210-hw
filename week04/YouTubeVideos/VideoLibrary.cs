dabf186d6ab35fab716df32bc5d2cddd458a2121
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-15 02:11:01 +0300

    effect

--- week04/YouTubeVideos/VideoLibrary.cs
+++ week04/YouTubeVideos/VideoLibrary.cs
@@ -1,15 +1,23 @@
 using System;
+using System.Collections.Generic;
 
-public class VideoLibrary
+ public class VideoLibrary
 {
+    private List<Video> _videos;
+    public VideoLibrary()
+    {
+        _videos = new List<Video>();
+    }
+
+    public void AddVideo(Video video)
+    {
+        _videos.Add(video);
+    }
+
+    public List<Video> GetVideos()
+    {
+        return _videos;
+    }
     
 
-
-
-
-
-
-
-
-    
 }
