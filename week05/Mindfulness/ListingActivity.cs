9f771efcdfb3b6f27f039a1f74af83f2de8e71ee
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-22 02:24:55 +0300

    effect

---
+++ week05/Mindfulness/ListingActivity.cs
@@ -0,0 +1,54 @@
+public class ListingActivity : Activity
+{private List<string> _prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who have you helped this week?",
+        "Who are your personal heroes?"
+    };private int _count;
+
+    public ListingActivity()
+        : base(
+            "Listing Activity",
+            "This activity will help you reflect on the good things in your life by having you list as many things as you can."
+          )
+    {
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        return _prompts[rand.Next(_prompts.Count)];
+    } public void Run()
+    {
+        DisplayStartingMessage();
+
+        Console.WriteLine("\nList as many responses as you can to the following prompt:");
+        Console.WriteLine($"--- {GetRandomPrompt()} ---");
+
+        Console.Write("You may begin in: ");
+        ShowCountdown(5);
+
+        List<string> items = new List<string>();
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.Write("> ");string item = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(item))
+            {
+                items.Add(item);
+            }
+        }
+
+        _count = items.Count;
+
+        Console.WriteLine($"\nYou listed {_count} items!");
+
+        DisplayEndingMessage();
+    }
+    
+
+
+}
---
+++ week05/Mindfulness/ReflectionActivity.cs
@@ -0,0 +1,66 @@
+using System;                                                                                                                                                         
+public class ReflectionActivity : Activity
+
+{
+    private List<string> _prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+    };
+
+    private List<string> _questions = new List<string>
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times?",
+        "What did you learn about yourself?",
+        "How can you keep this experience in mind in the future?"
+    };
+
+    public ReflectionActivity()
+        : base(
+            "Reflection Activity",
+            "This activity will help you reflect on times in your life when you have shown strength and resilience."
+          )
+    {
+    }
+
+    public string GetRandomPrompt()
+    {
+        Random rand = new Random();
+        return _prompts[rand.Next(_prompts.Count)];
+    }
+
+    public string GetRandomQuestion()
+    {
+        Random rand = new Random();
+        return _questions[rand.Next(_questions.Count)];
+    }
+
+    public void Run()
+    {
+        DisplayStartingMessage();
+
+        Console.WriteLine("\nConsider the following prompt:");
+        Console.WriteLine($"--- {GetRandomPrompt()} ---");
+
+        Console.WriteLine("\nWhen you have something in mind, press Enter.");
+        Console.ReadLine();
+
+        Console.WriteLine("Now ponder each of the following questions:");
+
+        DateTime endTime = DateTime.Now.AddSeconds(_duration);
+
+        while (DateTime.Now < endTime)
+        {
+            Console.WriteLine($"\n> {GetRandomQuestion()}");
+            ShowSpinner(5);
+        }
+
+        DisplayEndingMessage();
+    }
+}
