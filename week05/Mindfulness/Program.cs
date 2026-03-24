697eb4baff8b4eac3106f831fce5fd52db1beab5
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-03-25 01:20:57 +0300

    effect

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -25,7 +25,7 @@ class Program
             Console.Clear();
             Console.WriteLine("Menu Options:");
             Console.WriteLine("  1. Start breathing activity");
-            Console.WriteLine("  2. Start reflection activity");
+            Console.WriteLine("  2. Start reflecting activity");
             Console.WriteLine("  3. Start listing activity");
             Console.WriteLine("  4. Quit");
             Console.WriteLine();
