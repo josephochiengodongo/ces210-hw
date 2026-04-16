d8b77ccea2453fa3147b53e65078086f0ed8bf93
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-04-16 17:23:33 +0300

    effect

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,6 +1,6 @@
 public abstract class Activity
 {
-      private string _date;
+    private string _date;
     private int _minutes;
 
     public Activity(string date, int minutes)
@@ -17,7 +17,8 @@ public abstract class Activity
     public int GetMinutes()
     {
         return _minutes;
-    }  public abstract double GetDistance();
+    }
+    public abstract double GetDistance();
     public abstract double GetSpeed();
     public abstract double GetPace();
 
