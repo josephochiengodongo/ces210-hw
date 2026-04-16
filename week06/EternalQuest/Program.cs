5fae6fe13309f6e1719b6c395891c433c7e68f31
Joseph Ochieng Odongo <josephodongo99@gmail.com>
2026-04-16 16:19:15 +0300

    effect

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -1,21 +1,21 @@
-// 1.Added part of program: ReturdGoal.cs(meaning nagative purformance)
-//      (Addition of new class ReturdGoal.cs ) user loses points on failer to complete goal
+// 1.Added part of program: NegativeGoal.cs(meaning negative performance)
+//      (Addition of new class NegativeGoal.cs ) user loses points on failure to complete goal
 //  By:-Addition in GoalManager.cs -> menu in private void CreateGoal()
-//           Console.WriteLine("4. Returd Goal")
-//          _goals.Add(new ReturdGoal(name,desc,points) 
+//           Console.WriteLine("4. Negative Goal")
+//          _goals.Add(new NegativeGoal(name,desc,points) 
 //      -Addition in GoalManager.cs through private void RecordEvent()
 //               if  (_score < 0)
 //                {
 //                  _Score =+ 0
 //                }
- //     -Addition in GoalManager.cs through private void LoadGoals()
- //               else if (goalType == "ReturdGoal") 
- // 2. Added part of program: Level System
+//     -Addition in GoalManager.cs through private void LoadGoals()
+//               else if (goalType == "NegativeGoal") 
+// 2. Added part of program: Level System
 //      (Addition of new property _GetLevel in GoalManager.cs) user levels up after  
- //      reaching certain score  500,300 and 100 thresholds
+//      reaching certain score  500,300 and 100 thresholds
 //      By:-Addition in GoalManager.cs -> private int _GetLevel.
 //      -Addition in GoalManager.cs through public void DisplayPlayerInfo()       
-// 3. Added part of program: completedgoals counter
+// 3. Added part of program: completed goals counter
 //      (Addition of new property _completedGoals in GoalManager.cs) user can see how
 
 using System;
